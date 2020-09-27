2da0faad0eae58a47927f9e5178f2674e55ef738
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-09-27 11:14:46 +0900

    Second Commit

--- tabeleViewApp/AppDelegate.swift
+++ tabeleViewApp/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  tabeleViewApp
+//  MyReminderApp
 //
-//  Created by 金野利哉 on 2020/09/06.
-//  Copyright © 2020 金野利哉. All rights reserved.
+//  Created by たっくん on 2020/09/21.
+//  Copyright © 2020 tatsuya.com. All rights reserved.
 //
 
 import UIKit
@@ -13,14 +13,11 @@ import NotificationCenter
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
-    
-    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         sleep(2)
-        //Notification登録前のおまじない。テストの為、現在のノーティフケーションを削除します
+          //Notification登録前のおまじない。テストの為、現在のノーティフケーションを削除します
               UNUserNotificationCenter.current().removeAllPendingNotificationRequests();
               
               //Notification登録前のおまじない。これがないとpermissionエラーが発生するので必要です。
@@ -32,26 +29,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
               content.title = "防災アプリからのお知らせ";
               content.body = "災害への備えはできていますか？もう一度家族や友達ともう一度確認しましょう";
                 content.sound = UNNotificationSound.default
-            let date = DateComponents(day:1, hour:19, minute:30)
+            let date = DateComponents(day:1, hour:10, minute:16)
             let trigger = UNCalendarNotificationTrigger.init(dateMatching: date, repeats: true)
               let request = UNNotificationRequest.init(identifier: "TestNotification", content: content, trigger: trigger)
               let center = UNUserNotificationCenter.current()
               center.add(request)
               center.delegate = self
-        
-        // Override point for customization after application launch.
         return true
     }
     //上記のNotificatioを５秒後に受け取る関数
-      //ポップアップ表示のタイミングで呼ばれる関数
-      //（アプリがアクティブ、非アクテイブ、アプリ未起動,バックグラウンドでも呼ばれる）
-      func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                  willPresent notification: UNNotification,
-                                  withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-          completionHandler([.alert,.sound])
-      }
-          
-    
+       //ポップアップ表示のタイミングで呼ばれる関数
+       //（アプリがアクティブ、非アクテイブ、アプリ未起動,バックグラウンドでも呼ばれる）
+       func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                   willPresent notification: UNNotification,
+                                   withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+           completionHandler([.alert,.sound])
+       }
+           
+     
 
     // MARK: UISceneSession Lifecycle
 
@@ -66,6 +61,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 
 
 }
+
+
